b15ff8c97beefd031b1aca8d5eb03778f0e868f4
Mattia Carlino <matty_rhoin@live.it>
2025-05-18 01:04:22 +0200

    fix: update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer className="py-8 text-center text-neutral-500 text-sm">
-            © {new Date().getFullYear()} Mattia Carlino
+            © {new Date().getFullYear()} Designed by Mattia Carlino in Visual Studio Code. Built with React, Tailwind CSS, Vite, and styled using daisyUI. Deployed with care. Typeset in Inter.
         </footer>
     );
 }
